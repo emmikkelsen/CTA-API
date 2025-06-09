1e3e18b00a47c77ecf87887de68dc94bfe2c054c
emmikkelsen <emmikkelsen@gmail.com>
2025-06-08 19:15:35 -0500

    Add public init

--- Sources/CTA-API/Coordinate.swift
+++ Sources/CTA-API/Coordinate.swift
@@ -9,4 +9,9 @@
 public struct Coordinate: Sendable, Codable, Hashable {
     public let latitude: Double
     public let longitude: Double
+
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
